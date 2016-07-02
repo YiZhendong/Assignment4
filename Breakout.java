68c7b8f06c555325e2318fce2559aa28fd0c6142
S <s>
2016-07-02 10:36:47 +0800

    {"types":"C","runs":17}

--- Breakout.java
+++ Breakout.java
@@ -145,6 +145,6 @@ public class Breakout extends GraphicsProgram {
 
 	private boolean ballRightWall(GOval ball) {
 		// TODO Auto-generated method stub
-		return ball.getX()+BALL_RADIUS*2>=WIDTH;
+		return ball.getX()+BALL_RADIUS>=WIDTH;
 	}
 }
