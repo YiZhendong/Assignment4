bbd743b3da9406220ef4206e1a73e529efff3718
S <s>
2016-07-02 15:59:33 +0800

    {"types":"C","runs":1}

--- Breakout.java
+++ Breakout.java
@@ -165,6 +165,7 @@ public class Breakout extends GraphicsProgram {
 		}
 	}
 
+	/** when the ball collides the wall ,the ball turns */
 	private void turn(GOval ball) {
 		// TODO Auto-generated method stub
 		if(ballBelowFloor(ball)){
