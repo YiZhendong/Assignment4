53db5227e066bc478fc9693880ebfd15413f2d22
S <s>
2016-07-02 11:29:35 +0800

    {"types":"C","runs":26}

--- Breakout.java
+++ Breakout.java
@@ -96,7 +96,7 @@ public class Breakout extends GraphicsProgram {
 		// TODO Auto-generated method stub
 		for(int i = 0;i<NBRICKS_PER_ROW;i++){
 			int x = BRICK_SEP*(i+1) +i * BRICK_WIDTH;
-			int y = BRICK_Y_OFFSET + ;
+			int y = BRICK_Y_OFFSET + row*BRICK_HEIGHT;
 		}
 	}
 
