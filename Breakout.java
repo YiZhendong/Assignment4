8f6b1a8df98fdf20a2cd7dd5cc3b075f896728ed
S <s>
2016-07-02 11:21:07 +0800

    {"types":"C","runs":26}

--- Breakout.java
+++ Breakout.java
@@ -57,7 +57,7 @@ public class Breakout extends GraphicsProgram {
 	/** Number of turns */
 	private static final int NTURNS = 3;
 	
-	private static final Color colors={red,orange,yellow,green,blue};
+	private static final Color colors={Color.red,Color.orange,Color.yellow,Color.green,Color.blue};
 
 	public void run() {
 		createPaddle();
